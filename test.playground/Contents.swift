df85af4ae15622f8144b2d19cd80404540817dce
Administrator <administrator@Leeerooy.local>
2016-02-16 17:08:50 -0800

    help

---
+++ test.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+let swiftTeam = 13
+let iOSTeam = 54
+let otherTeams = 48
+let totalTeam = swiftTeam + iOSTeam + otherTeams
+
+print( "\(iOSTeam) members on the iOS team!")
+print( "\(otherTeams) members on other teams")
